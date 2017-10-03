473fc035469fa23059c85b1251206fd00f530acf
Tom van Nimwegen <tomvannimwegen@gmail.com>
2017-10-03 16:50:45 +0200

    Switched Core to .NET Framework
    - midi lib was not compatible with .NET Standard

---
+++ CoreLib/Builder/SheetBuidler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Builder
+{
+    class SheetBuidler
+    {
+    }
+}
---
+++ CoreLib/IO/Internal/MidiMusicReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+using Sanford.Multimedia.Midi;
+using System.Threading;
+
+namespace Core.IO.Internal
+{
+    [MusicReader("(.mid|.midi)", nameof(MidiMusicReader))]
+    class MidiMusicReader : IMusicReader
+    {
+        private SemaphoreSlim mse = new SemaphoreSlim(0, 1);
+
+        public async Task<Sheet> ReadFromFileAsync(string filePath, string name = null)
+        {
+            var sequence = new Sequence();
+            sequence.LoadCompleted += (s, e) => mse.Release();
+
+            sequence.LoadAsync(filePath);
+
+            await mse.WaitAsync();
+
+            // TODO: do something here...
+
+            mse = new SemaphoreSlim(0, 1);
+
+            return new Sheet();
+        }
+    }
+}
