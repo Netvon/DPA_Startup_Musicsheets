b6b528e86ce852e05761a0bd6413e2e104e1c137
Tom van Nimwegen <tomvannimwegen@gmail.com>
2018-01-04 16:51:05 +0100

    updated notehandler for lilypond

--- CoreLib/Builder/Internal/Lilypond/NoteHandler.cs
+++ CoreLib/Builder/Internal/Lilypond/NoteHandler.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Core.Builder.Internal.Lilypond
@@ -11,14 +12,49 @@ namespace Core.Builder.Internal.Lilypond
     {
         readonly string[] names = Enum.GetNames(typeof(NotePitch));
 
+        List<MSNote> currentBar = new List<MSNote>();
+
         public bool Accepts(string token)
         {
-            return names.Any(x => x == token[0].ToString().ToUpper()) && token.Length > 1;
+            return token.Contains("|") || (names.Any(x => x == token[0].ToString().ToUpper()) && token.Length > 1);
         }
 
         public void Handle(string token, SheetBuilder builder)
         {
-            throw new NotImplementedException();
+            if (token.Contains("|"))
+            {
+                builder.AddBar(currentBar);
+                currentBar.Clear();
+            }
+            else
+            {
+                var noteBuidler = builder.GetNoteBuilder();
+
+                if (token[0] == 'r')
+                {
+                    noteBuidler.AddRest();
+                }
+                else
+                {
+                    var pitch = (NotePitch)Enum.Parse(typeof(NotePitch), token[0].ToString().ToUpper());
+                    noteBuidler.AddPitch(pitch);
+                }
+
+                int up = token.Count(x => x == '\'');
+                int down = token.Count(x => x == ',');
+                int dots = token.Count(x => x == '.');
+
+                var numberMatches = Regex.Match(token, @"(\d)");
+
+                var length = int.Parse(numberMatches.Groups[0].Value);
+
+                noteBuidler.AddDotts((uint)dots);
+                noteBuidler.AddOctave(up + down);
+                noteBuidler.AddBaseLength((uint)length);
+
+                currentBar.Add(noteBuidler.Build());
+            }
+
         }
     }
 }
