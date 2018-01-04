6391ecd3c56587511648e33e9a43e85ee12ef9d3
Tom van Nimwegen <tomvannimwegen@gmail.com>
2018-01-04 16:34:47 +0100

    Started on Ly note handler

---
+++ CoreLib/Builder/Internal/Lilypond/NoteHandler.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Builder.Internal.Lilypond
+{
+    class NoteHandler : ILilypondTokenHandler
+    {
+        readonly string[] names = Enum.GetNames(typeof(NotePitch));
+
+        public bool Accepts(string token)
+        {
+            return names.Any(x => x == token[0].ToString().ToUpper()) && token.Length > 1;
+        }
+
+        public void Handle(string token, SheetBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
