d4ade9a03a28af0b2d047797ef3d13f39e7e75a7
Tom van Nimwegen <tomvannimwegen@gmail.com>
2018-01-07 21:45:19 +0100

    Started on commands

---
+++ CoreLib/Editor/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using Core.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Commands
+{
+    public interface ICommand
+    {
+        void Invoke(ref State state);
+        void Undo(ref State state);
+    }
+}
---
+++ CoreLib/Editor/Internal/UndoCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Editor.Internal
+{
+    [Command("undo")]
+    class UndoCommand : Command
+    {
+
+    }
+}
