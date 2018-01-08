775ee4f446706d50d7afbe7524c6b822f54b6e20
Tom van Nimwegen <tomvannimwegen@gmail.com>
2018-01-08 20:03:48 +0100

    Added more tests

--- CoreTests/CommandTests.cs
+++ CoreTests/CommandTests.cs
@@ -10,13 +10,25 @@ namespace CoreTests
     public class CommandTests
     {
         public const string TestCommandName = "test";
+        public const string HalloCommandName = "hallo";
+
+        public static string Value { get; set; }
 
         [CommandBinding(Name = TestCommandName)]
         class TestCommand : ICommand
         {
             public void Invoke()
             {
-                Console.WriteLine("Hallo!");
+                Value = TestCommandName;
+            }
+        }
+
+        [CommandBinding(Name = HalloCommandName)]
+        class HalloCommand : ICommand
+        {
+            public void Invoke()
+            {
+                Value = HalloCommandName;
             }
         }
 
@@ -25,9 +37,18 @@ namespace CoreTests
         {
             var commands = new Commands(Assembly.GetAssembly(typeof(CommandTests)));
             commands.AddBinding(TestCommandName, "(AltLeft|AltRight) C");
+            commands.AddBinding(HalloCommandName, "(AltLeft|AltRight) T");
 
             commands.Handle("AltRight");
             Assert.IsTrue(commands.Handle("C"));
+            Assert.AreEqual(TestCommandName, Value);
+
+            Assert.IsFalse(commands.Handle("T"));
+            Assert.AreEqual(TestCommandName, Value);
+
+            commands.Handle("AltLeft");
+            Assert.IsTrue(commands.Handle("T"));
+            Assert.AreEqual(HalloCommandName, Value);
         }
     }
 }
