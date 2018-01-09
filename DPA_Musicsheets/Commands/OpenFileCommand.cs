d4da7e61f80e169ac10311acf66bd175c5f0f193
ViezeFredsel <wngeck@avans.nl>
2018-01-09 09:43:07 +0100

    rename

---
+++ DPA_Musicsheets/Commands/OpenFileCommand.cs
@@ -0,0 +1,21 @@
+﻿using Core.Commands;
+using Core.Memento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Commands
+{
+    class OpenFileCommand : ICommand
+    {
+        public bool CanInvoke<T>(CareTaker<T> careTaker) => true;
+
+        public void Invoke<T>(CareTaker<T> careTaker)
+        {
+            
+
+        }
+    }
+}
